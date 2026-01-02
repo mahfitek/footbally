20b3ac8e7343f9ce62f623b53e09dd0b153813ea
mahfitek <osman.temur0498@gmail.com>
2026-01-03 02:06:21 +0300

    fix env access for vercel api

--- api/send-email.ts
+++ api/send-email.ts
@@ -1,6 +1,7 @@
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env['RESEND_API_KEY']);
+
 
 export default async function handler(req: any, res: any) {
   if (req.method !== 'POST') {
